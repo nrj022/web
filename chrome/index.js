016f0c555a86d069a3fb26b9c9d0ef80f3f64d27
nrj022 <72160826+nrj022@users.noreply.github.com>
2021-04-30 14:06:21 +0900

    make google app menu and control menu by click

--- chrome/index.js
+++ chrome/index.js
@@ -1,37 +1,52 @@
-/* document.querySelector("input") {
-  document.querySelector("body").style.backgroundColor = "red";
-}); */
-/* function clickHandler() {
-  console.log(document.activeElement.tagName);
-}*/
-/* window.addEventListener("click", clickHandler); */
+const googleAppMenu = document.querySelector(".menu-dots__bg");
 const googleUserInfo = document.querySelector(".profile__bg");
-
-/* function init() {
-  googleUserInfo.onmouseover = function () {
-    console.log("hi");
-  };
-}
- */
-
-/* const i = document.getElementsByClassName("user-info");
-i.style.backgroundColor = "red"; */
+const googleApps = document.querySelector(".google-apps");
 const userInfo = document.querySelector(".user-info");
 
-function userInfoVisibility() {
+/* function menuVisibility(event) {
+  menu.style.visibility = "hidden";
+  menu.style.visibility = "hidden";
+  const target = event.target;
+  var menu;
+  if (target == googleUserInfo) {
+    menu = userInfo;
+  } else if (target == googleAppMenu) {
+    menu = googleApps;
+  }
+  console.log(menu);
+  if (menu.style.visibility === "visible") {
+    menu.style.visibility = "hidden";
+  } else {
+    menu.style.visibility = "visible";
+  }
+} */
+
+function infoMenuVisibility() {
   if (userInfo.style.visibility === "visible") {
     userInfo.style.visibility = "hidden";
   } else {
     userInfo.style.visibility = "visible";
+    // check the overwrap
+    if (googleApps.style.visibility == "visible") {
+      googleApps.style.visibility = "hidden";
+    }
   }
 }
 
-/* function userInfoHidden() {
-  if (userInfo.style.visibility === "visible") {
-    userInfo.style.visibility = "hidden";
-    console.log("hi");
+function googleMenuVisibility() {
+  if (googleApps.style.visibility === "visible") {
+    googleApps.style.visibility = "hidden";
+  } else {
+    googleApps.style.visibility = "visible";
+    // check the overwrap
+    if (userInfo.style.visibility == "visible") {
+      userInfo.style.visibility = "hidden";
+    }
   }
-} */
+}
+function init() {
+  googleUserInfo.addEventListener("click", infoMenuVisibility);
+  googleAppMenu.addEventListener("click", googleMenuVisibility);
+}
 
-googleUserInfo.addEventListener("click", userInfoVisibility);
-/* document.addEventListener("click", userInfoHidden); */
+init();
