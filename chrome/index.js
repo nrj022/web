5d83c9d1d189668fd3310893128ca8b163b49714
nrj022 <72160826+nrj022@users.noreply.github.com>
2021-04-30 18:15:03 +0900

    app menu and profile menu hover and animation

--- chrome/index.js
+++ chrome/index.js
@@ -1,52 +1,74 @@
-const googleAppMenu = document.querySelector(".menu-dots__bg");
-const googleUserInfo = document.querySelector(".profile__bg");
-const googleApps = document.querySelector(".google-apps");
+const appMenu = document.querySelector(".menu-dots__bg");
+const profileMenu = document.querySelector(".profile__bg");
+const apps = document.querySelector(".google-apps");
 const userInfo = document.querySelector(".user-info");
 
-/* function menuVisibility(event) {
-  menu.style.visibility = "hidden";
-  menu.style.visibility = "hidden";
-  const target = event.target;
-  var menu;
-  if (target == googleUserInfo) {
-    menu = userInfo;
-  } else if (target == googleAppMenu) {
-    menu = googleApps;
-  }
-  console.log(menu);
-  if (menu.style.visibility === "visible") {
-    menu.style.visibility = "hidden";
+const appMenuAlert = document.querySelector(".menu-dots__alert");
+const profileMenuAlert = document.querySelector(".profile__alert");
+/* const menuAlertFadeIn = document.querySelector("fadeIn"); */
+
+/* var appMenuAlertAni = appMenuAlert.animate(
+  {
+    visibility: ["hidden", "visible"],
+  },
+  2000
+);
+var profileMenuAlertAni = appMenuAlert.animate({}); */
+
+function appMenuVisibility() {
+  if (apps.style.visibility === "visible") {
+    apps.style.visibility = "hidden";
+    appMenu.style.backgroundColor = "";
   } else {
-    menu.style.visibility = "visible";
+    apps.style.visibility = "visible";
+    appMenu.style.backgroundColor = "#85858567";
+    // prevent the overwrap
+    if (userInfo.style.visibility === "visible") {
+      userInfo.style.visibility = "hidden";
+      profileMenu.style.backgroundColor = "";
+    }
   }
-} */
+}
 
 function infoMenuVisibility() {
   if (userInfo.style.visibility === "visible") {
     userInfo.style.visibility = "hidden";
+    profileMenu.style.backgroundColor = "";
   } else {
     userInfo.style.visibility = "visible";
-    // check the overwrap
-    if (googleApps.style.visibility == "visible") {
-      googleApps.style.visibility = "hidden";
+    profileMenu.style.backgroundColor = "#85858567";
+    // prevent the overwrap
+    if (apps.style.visibility === "visible") {
+      apps.style.visibility = "hidden";
+      appMenu.style.backgroundColor = "";
     }
   }
 }
 
-function googleMenuVisibility() {
-  if (googleApps.style.visibility === "visible") {
-    googleApps.style.visibility = "hidden";
-  } else {
-    googleApps.style.visibility = "visible";
-    // check the overwrap
-    if (userInfo.style.visibility == "visible") {
-      userInfo.style.visibility = "hidden";
-    }
-  }
+function menuAlertFadeIn(menuAlert) {
+  menuAlert.style.animationName = "fadein";
 }
+
+function menuAlertFadeOut(menuAlert) {
+  menuAlert.style.animationName = "";
+}
+
 function init() {
-  googleUserInfo.addEventListener("click", infoMenuVisibility);
-  googleAppMenu.addEventListener("click", googleMenuVisibility);
+  profileMenu.addEventListener("click", infoMenuVisibility);
+  appMenu.addEventListener("click", appMenuVisibility);
+
+  profileMenu.addEventListener("mouseover", function () {
+    menuAlertFadeIn(profileMenuAlert);
+  });
+  profileMenu.addEventListener("mouseout", function () {
+    menuAlertFadeOut(profileMenuAlert);
+  });
+  appMenu.addEventListener("mouseover", function () {
+    menuAlertFadeIn(appMenuAlert);
+  });
+  appMenu.addEventListener("mouseout", function () {
+    menuAlertFadeOut(appMenuAlert);
+  });
 }
 
 init();
