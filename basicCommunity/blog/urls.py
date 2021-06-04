6cf3213fb86ad4c0f629516cce88f5127e481367
nrj022 <72160826+nrj022@users.noreply.github.com>
2021-06-05 02:42:46 +0900

    Styling post detail page

--- basicCommunity/blog/urls.py
+++ basicCommunity/blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.main_board, name='main_board'),]
+urlpatterns = [
+    path('', views.main_board, name='main_board'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
