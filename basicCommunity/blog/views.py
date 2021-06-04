c5266e2410044b2150a3aadd799808871ff64740
nrj022 <72160826+nrj022@users.noreply.github.com>
2021-06-04 23:48:01 +0900

    Represent Post title, author and date

--- basicCommunity/blog/views.py
+++ basicCommunity/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def main_board(request):
-    return render(request, 'blog/main_board.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/main_board.html', {'posts': posts})
