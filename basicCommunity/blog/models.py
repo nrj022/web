7992ff1d7a620574559f85f5365998bbb4cd6df6
nrj022 <72160826+nrj022@users.noreply.github.com>
2021-06-08 01:14:22 +0900

    Chango Animal cate to Coding cate

--- basicCommunity/blog/models.py
+++ basicCommunity/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default = "coding")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
